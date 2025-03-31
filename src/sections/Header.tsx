53a2d42dae081940c46fcfc128a771c59fe732c2
Gaurav Raj <techw803@gmail.com>
2025-03-31 20:41:10 +0530

    Hero Section done...

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 const HeaderSection = () => {
   return (
-    <div className="flex justify-center items-center fixed top-3 w-full">
+    <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a className="nav-item" href="#">
           Home
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,18 +3,60 @@ import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
+import CircleIcon from "@/assets/icons/chrome.svg";
 
 const HeroSection = () => {
   return (
+    // background and rings - start
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div
-        className="absolute inset-0 -z-30 opacity-5"
-        style={{ backgroundImage: `url(${grainImage.src})` }}
-      ></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{ backgroundImage: `url(${grainImage.src})` }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        {/*background and rings - end*/}
+
+        {/*stars - start here*/}
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+
+        <HeroOrbit size={720} rotation={85}>
+          <CircleIcon className="size-3 rounded-full text-emerald-300/15" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <CircleIcon className="size-2 rounded-full text-emerald-300/15" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <CircleIcon className="size-2 rounded-full text-emerald-300/15" />
+        </HeroOrbit>
+        {/*stars - ends here*/}
+      </div>
 
       <div className="container mx-auto">
         <div className="flex flex-col items-center">
