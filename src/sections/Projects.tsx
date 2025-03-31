fb98054b41eb5d51f0bdc85716048a2bc3cd714b
Gaurav Raj <techw803@gmail.com>
2025-04-01 04:00:06 +0530

    fixed import but unused error

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,7 +7,6 @@ import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
