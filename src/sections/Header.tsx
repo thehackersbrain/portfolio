1f797aef15b7b0e062b97ce5489121ad2dd4b3b2
Gaurav Raj <techw803@gmail.com>
2025-03-31 19:24:03 +0530

    Hero Section almost done...

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,25 +1,28 @@
 const HeaderSection = () => {
-    return (
-        <div className="flex justify-center items-center fixed top-3 w-full">
-            <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-                <a className="nav-item" href="#">
-                    Home
-                </a>
-                <a className="nav-item" href="#">
-                    About
-                </a>
-                <a className="nav-item" href="#">
-                    Projects
-                </a>
-                <a className="nav-item" href="#">
-                    Contact
-                </a>
-                <a className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" href="#">
-                    Resume
-                </a>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center fixed top-3 w-full">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a className="nav-item" href="#">
+          Home
+        </a>
+        <a className="nav-item" href="#">
+          About
+        </a>
+        <a className="nav-item" href="#">
+          Projects
+        </a>
+        <a className="nav-item" href="#">
+          Contact
+        </a>
+        <a
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+          href="#"
+        >
+          Resume
+        </a>
+      </nav>
+    </div>
+  );
 };
 
 export default HeaderSection;
