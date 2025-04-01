1b2661fd0a076b2d67864efcd903f9f14b4c502d
Gaurav Raj <techw803@gmail.com>
2025-04-02 03:25:50 +0530

    animation done - about section

--- src/components/ToolBoxItems.tsx
+++ src/components/ToolBoxItems.tsx
@@ -1,5 +1,6 @@
 import TechIcon from "@/components/TechIcon";
 import { twMerge } from "tailwind-merge";
+import { Fragment } from "react";
 
 const ToolBoxItems = ({
   items,
@@ -21,14 +22,18 @@ const ToolBoxItems = ({
           itemsWrapperClassName,
         )}
       >
-        {items.map((item) => (
-          <div
-            key={item.title}
-            className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
-          >
-            <TechIcon component={item.iconType} />
-            <span className="font-semibold">{item.title}</span>
-          </div>
+        {[...new Array(2)].fill(0).map((_, i) => (
+          <Fragment key={i}>
+            {items.map((item) => (
+              <div
+                key={item.title}
+                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+              >
+                <TechIcon component={item.iconType} />
+                <span className="font-semibold">{item.title}</span>
+              </div>
+            ))}
+          </Fragment>
         ))}
       </div>
     </div>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SectionHeader } from "@/components/SectionHeader";
 import Card from "@/components/Card";
 import BookImage from "@/assets/images/book-cover.png";
@@ -12,6 +14,8 @@ import MapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import CardHeader from "@/components/CardHeader";
 import ToolBoxItems from "@/components/ToolBoxItems";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const toolboxItems = [
   {
@@ -100,6 +104,8 @@ const hobbies = [
 ];
 
 const AboutSection = () => {
+  const constraintRef = useRef(null);
+
   return (
     <div className="py-20 lg:py-28 px-3">
       <div className="container mx-auto">
@@ -125,11 +131,15 @@ const AboutSection = () => {
                 description="Explore the technologies and tools I use to craft exceptional digital experiences."
                 className=""
               />
-              <ToolBoxItems items={toolboxItems} className="" />
+              <ToolBoxItems
+                items={toolboxItems}
+                className=""
+                itemsWrapperClassName="animate-move-left"
+              />
               <ToolBoxItems
                 items={toolboxItems}
                 className="mt-6"
-                itemsWrapperClassName="-translate-x-1/2"
+                itemsWrapperClassName="animate-move-right [animation-duration:15s]"
               />
             </Card>
           </div>
@@ -140,21 +150,23 @@ const AboutSection = () => {
                 description="Explore my interests and hobbies beyond the digital realm."
                 className="px-6 py-6"
               />
-              <div className="relative flex-1">
+              <div className="relative flex-1" ref={constraintRef}>
                 {hobbies.map((hobby) => (
-                  <div
+                  <motion.div
                     key={hobby.title}
                     className="inline-flex items-center gap-2 px-6 primary-gradient rounded-full py-1.5 absolute"
                     style={{
                       left: hobby.left,
                       top: hobby.top,
                     }}
+                    drag
+                    dragConstraints={constraintRef}
                   >
                     <span className="font-medium text-gray-950">
                       {hobby.title}
                     </span>
                     <span>{hobby.emoji}</span>
-                  </div>
+                  </motion.div>
                 ))}
               </div>
             </Card>
@@ -164,7 +176,9 @@ const AboutSection = () => {
                 alt="map"
                 className="w-full h-full object-cover"
               />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full primary-gradient after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <div className="absolute inset-0 rounded-full primary-gradient -z-20 animate-ping [animation-duration:2s]"></div>
+                <div className="absolute inset-0 rounded-full primary-gradient -z-10"></div>
                 <Image src={smileMemoji} alt="smiley" className="size-20" />
               </div>
             </Card>
