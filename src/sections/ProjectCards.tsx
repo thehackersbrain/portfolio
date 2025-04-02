6bb6f61f690bbb96a2c1ea7915d3c532ad24b90e
Gaurav Raj <techw803@gmail.com>
2025-04-02 23:22:05 +0530

    added projects section - updated

--- src/sections/ProjectCards.tsx
+++ src/sections/ProjectCards.tsx
@@ -1,94 +1,269 @@
-"use client";
-import Image from "next/image";
-import React from "react";
-import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
+import { CardSpotlight } from "@/components/ui/card-spotlight";
 import { SectionHeader } from "@/components/SectionHeader";
+import { Folder, GithubIcon, SquareArrowOutUpRight } from "lucide-react";
+import Link from "next/link";
 
-export function ProjectCardsSection() {
-  const cards = data.map((card, index) => (
-    <Card key={card.src} card={card} index={index} />
-  ));
-
+export function CardSpotlightDemo() {
   return (
     <div className="py-16 lg:py-24">
       <div className="container mx-auto">
         <SectionHeader
           title="Other Noteworthy Projects"
-          eyebrow="Honable Mentions"
+          eyebrow="Honorable Mentions"
           description="Don&#39;t just take my word for it. See what my clients have to say
         about my work."
         />
-        <div className="fade-sideways">
-          <Carousel items={cards} />
+        <div className="mt-20 flex flex-col gap-3 items-center">
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-5 lg:grid-cols-3">
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+            <CardSpotlight className="h-fit w-80 md:col-span-1 lg:col-span-1 float-grow-an hover:z-20 hover:relative">
+              <div className="relative z-20">
+                <div className="grid grid-cols-5 items-center mb-4">
+                  <Folder className="col-span-3 size-14" />
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <GithubIcon className="size-5" />
+                  </Link>
+                  <Link
+                    href="https://github.com/thehackersbrain/snapgram"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="col-span-1 justify-self-end float-up-an"
+                  >
+                    <SquareArrowOutUpRight className="size-5" />
+                  </Link>
+                </div>
+                <div className="w-full">
+                  <p className="text-xl font-bold mt-1 text-white">
+                    Authentication steps
+                  </p>
+                  <div className="text-neutral-200 text-sm mt-4 relative z-20">
+                    Follow these steps to secure your account:
+                    <ul className="list-none  mt-2">
+                      <li>Enter your email address</li>
+                      <li>Create a strong password</li>
+                      <li>Set up two-factor authentication</li>
+                      <li>Verify your identity</li>
+                    </ul>
+                  </div>
+                  <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+                    Ensuring your account is properly secured helps protect your
+                    personal information and data.
+                  </p>
+                </div>
+              </div>
+            </CardSpotlight>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-const DummyContent = () => {
-  return (
-    <>
-      <div
-        key={"dummy-content"}
-        className="bg-gray-800/98 p-8 md:p-14 rounded-3xl mb-4"
-      >
-        <p className="text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
-          <span className="font-bold text-neutral-200">
-            The first rule of Apple club is that you boast about Apple club.
-          </span>{" "}
-          Keep a journal, quickly jot down a grocery list, and take amazing
-          class notes. Want to convert those notes to text? No problem.
-          Langotiya jeetu ka mara hua yaar is ready to capture every thought.
-        </p>
-        <Image
-          src="https://assets.aceternity.com/macbook.png"
-          alt="Macbook mockup from Aceternity UI"
-          height="500"
-          width="500"
-          className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
-        />
-      </div>
-    </>
-  );
-};
-
-const data = [
-  {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
-    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Productivity",
-    title: "Enhance your productivity.",
-    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Product",
-    title: "Launching the new Apple Vision Pro.",
-    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-
-  {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "iOS",
-    title: "Photography just got better.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-];
