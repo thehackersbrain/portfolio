e77eb0fdb566ce08cfadaa53c26aa9a6a74aa725
Gaurav Raj <techw803@gmail.com>
2025-04-06 20:39:43 +0530

    started projects page...

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,23 @@
+import { ExpandableCardDemo } from "@/sections/ProjectsList";
+import { SectionHeader } from "@/components/SectionHeader";
+
 const ProjectsPage = () => {
   return (
-    <section className="h-screen my-16 lg:my-24">
-      <div className="text-center text-3xl font-serif">Projects Page</div>
+    <section
+      className="pb-16 lg:py-24 my-16 lg:my-3"
+      id="projects"
+      aria-label="Projects"
+    >
+      <div className="container mx-auto">
+        <SectionHeader
+          title="Featured Projects"
+          eyebrow="1. Real-world Results"
+          description="Innovative Exploits & Ingenious Builds - A Showcase of My Finest Creations."
+        />
+        <div className="mt-10">
+          <ExpandableCardDemo />
+        </div>
+      </div>
     </section>
   );
 };
