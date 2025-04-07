28addd212e8e4f50af1d15b8248c798fb2b16f95
Gaurav Raj <techw803@gmail.com>
2025-04-07 20:23:41 +0530

    experience section almost done -- projects page...

--- src/sections/projects/Experience.tsx
+++ src/sections/projects/Experience.tsx
@@ -5,11 +5,11 @@ import React from "react";
 const ExperienceSectionP = () => {
   return (
     <div className="flex flex-col sm:grid sm:grid-cols-4 gap-3 my-4 mb-3 md:mb-0 lg:mb-0 border-b-1 py-3 border-[#334155] w-full">
-      <h3 className="sm:col-span-1 text-xl font-bold font-altfont flex flex-row">
+      <h3 className="sm:col-span-1 text-xl font-bold font-altfont flex flex-row overflow-x-clip">
         Experience
       </h3>
       <div className="sm:col-span-3 inline-flex flex-col">
-        <div className="sm:inline-grid mb-4 inline-flex flex-col">
+        <div className="sm:inline-grid mb-6 inline-flex flex-col">
           <div className="sm:grid sm:grid-cols-4 flex flex-col">
             <div className="col-span-2">
               <span className="font-semibold">Python Developer</span>
@@ -28,87 +28,103 @@ const ExperienceSectionP = () => {
                 variant="outline"
                 className="outline outline-white/25 text-white/50"
               >
-                Testing
+                Sales
               </Badge>
               <Badge
                 variant="outline"
                 className="outline outline-white/25 text-white/50"
               >
-                Testing
+                Market Research
               </Badge>
               <Badge
                 variant="outline"
                 className="outline outline-white/25 text-white/50"
               >
-                Testing
+                Client Relations
               </Badge>
               <Badge
                 variant="outline"
                 className="outline outline-white/25 text-white/50"
               >
-                Testing
+                Market Data
               </Badge>
             </div>
           </div>
           <div>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
+            <ul className="list-disc px-3 text-sm text-white/60">
+              <li>
+                Developed Python scripts for automating internal security tasks
+                and vulnerability assessments.
+              </li>
+              <li>
+                Collaborated with researchers to create proof-of-concept
+                exploits and payloads.
+              </li>
+              <li>
+                Built and maintained internal tools for pentesting and bug
+                bounty workflows.
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="sm:col-span-3 inline-flex flex-col">
-          <div className="sm:inline-grid mb-4 inline-flex flex-col">
-            <div className="sm:grid sm:grid-cols-4 flex flex-col">
-              <div className="col-span-2">
-                <span className="font-semibold">Python Developer</span>
-                <p className="text-sm text-white/80">Cybercraft Labs Pvt Ltd</p>
-              </div>
-              <div className="col-span-2 flex flex-row gap-2 items-center justify-end text-white/60">
-                <MapPin className="size-4" />
-                <p className="text-xs ">Patna, Bihar</p>
-                <CalendarDays className="size-4" />
-                <p className="text-xs">Sept 2023 - Nov 2024</p>
-              </div>
+        <div className="sm:inline-grid mb-6 inline-flex flex-col">
+          <div className="sm:grid sm:grid-cols-4 flex flex-col">
+            <div className="col-span-2">
+              <span className="font-semibold">Market Representative</span>
+              <p className="text-sm text-white/80">
+                Nouveau Medicament Private Limited
+              </p>
             </div>
-            <div className="my-3 gap-3 max-w-screen">
-              <div className="flex flex-row gap-3 items-center justify-start">
-                <Badge
-                  variant="outline"
-                  className="outline outline-white/25 text-white/50"
-                >
-                  Testing
-                </Badge>
-                <Badge
-                  variant="outline"
-                  className="outline outline-white/25 text-white/50"
-                >
-                  Testing
-                </Badge>
-                <Badge
-                  variant="outline"
-                  className="outline outline-white/25 text-white/50"
-                >
-                  Testing
-                </Badge>
-                <Badge
-                  variant="outline"
-                  className="outline outline-white/25 text-white/50"
-                >
-                  Testing
-                </Badge>
-              </div>
+            <div className="col-span-2 flex flex-row gap-2 items-center justify-end text-white/60">
+              <MapPin className="size-4" />
+              <p className="text-xs ">Patna, Bihar</p>
+              <CalendarDays className="size-4" />
+              <p className="text-xs">Apr 2023 - Sept 2023</p>
             </div>
-            <div>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
-              <p>test</p>
+          </div>
+          <div className="my-3 gap-3 max-w-screen">
+            <div className="flex flex-row gap-3 items-center justify-start">
+              <Badge
+                variant="outline"
+                className="outline outline-white/25 text-white/50"
+              >
+                Python
+              </Badge>
+              <Badge
+                variant="outline"
+                className="outline outline-white/25 text-white/50"
+              >
+                Automation
+              </Badge>
+              <Badge
+                variant="outline"
+                className="outline outline-white/25 text-white/50"
+              >
+                Cybersecurity
+              </Badge>
+              <Badge
+                variant="outline"
+                className="outline outline-white/25 text-white/50"
+              >
+                Internal API
+              </Badge>
             </div>
+          </div>
+          <div>
+            <ul className="list-disc px-3 text-sm text-white/60">
+              <li>
+                Represented the company in regional medical product promotions
+                and demos.
+              </li>
+              <li>
+                Conducted field research and competitor analysis to improve
+                outreach.
+              </li>
+              <li>
+                Managed relationships with local pharmacies and distributors to
+                expand the sales network.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
